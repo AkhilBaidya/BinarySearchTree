f53738125f61c808f81ddf21fcb04b70cfb97c88
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-11 09:40:51 -0700

    Making node class

--- node.h
+++ node.h
@@ -4,5 +4,24 @@
 using namespace std;
 
 class node {
+
+ public:
+  node();
+  node(data);
+  ~node();
+
+  //set
+  void setL(node*);
+  void setR(node*);
+  void setData(int);
+
+  //get
+  node* getL();
+  node* getR();
+  int getData();
   
+ private:
+  node* left;
+  node* right;
+  int data;
 }
