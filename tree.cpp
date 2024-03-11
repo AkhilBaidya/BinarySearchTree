d3b1bfb2413d021c93558790a1be961e80fd4e05
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-11 09:58:45 -0700

    Added prompts from program to enter each of the functions

--- tree.cpp
+++ tree.cpp
@@ -10,6 +10,35 @@ void search(node*, int);
 void del(node*, int);
 
 int main {
+
+  bool editing = true;
+  char choice[15];
+
+  while (editing) {
+    cout << "Hi, there. Would you like to create, print, delete_from, or search a Binary Tree? Or quit the program? " << endl;
+    cin >> choice;
+
+    if (!strcmp(choice, "quit")) {
+      editing = false;
+    }
+
+    else if (!strcmp(choice, "create")) {
+
+    }
+
+    else if (!strcmp(choice, "print")) {
+
+    }
+
+    else if (!strcmp(choice, "delete_from")) {
+
+    }
+
+    else if (!strcmp(choice, "search")) {
+
+    }
+    
+  }
   return 0;
 }
 
