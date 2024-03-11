0d2c81a8bfde1f20575b20bc686f0e2d1b465e2d
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-03-11 09:45:11 -0700

    Defining constructor and destructor

--- node.cpp
+++ node.cpp
@@ -5,13 +5,42 @@
 using namespace std;
 
 node::node() {
-
+  left = NULL;
+  right = NULL;
+  data = 0;
 }
 
-node::node(int data) {
-
+node::node(int newData) {
+  left = NULL;
+  right = NULL;
+  data = newData;
 }
 
 node::~node() {
+  delete left;
+  delete right;
+}
+
+void node::setL(node* child) {
+
+}
+
+void node::setR(node* child) {
+
+}
+
+void node::setData(int newData) {
+
+}
+
+node* node::getL() {
+
+}
+
+node* node::getR() {
+
+}
+
+int node::getData() {
 
 }
