3cd5ef8249da36c15b6c73860c706767f0b854dd
Akhil Baidya <akhilbaidya101@gmail.com>
2024-03-19 20:39:12 -0700

    Final comments

--- node.h
+++ node.h
@@ -1,3 +1,14 @@
+/*
+C++ Data Structures: Node Class Header by Akhil Baidya
+
+Submission Date: 3/20/24
+
+Notes:
+This is the header file for the node class. It lists the values and functions in a node.
+A node contains data (as an integer) and a pointer to a left and right node.
+It additionally will have functions (on top of a constructor and destructor) to set and get these values.
+ */
+
 #include <iostream>
 #include <cstring>
 
@@ -6,21 +17,26 @@ using namespace std;
 class node {
 
  public:
+
+  //Constructors:
   node();
   node(int);
+
+  //Destructor:
   ~node();
 
-  //set
-  void setL(node*);
-  void setR(node*);
+  //Set functions:
+  void setL(node*); //set left node
+  void setR(node*); //set right node
   void setData(int);
 
-  //get
-  node* getL();
-  node* getR();
+  //Get functions
+  node* getL(); //get left node
+  node* getR(); //get right node
   int getData();
   
  private:
+  //Values:
   node* left;
   node* right;
   int data;
