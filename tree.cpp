c811a49107e0879a270b0ada587653f66f568ad2
Akhil Baidya <akhilbaidya101@gmail.com>
2024-03-19 20:33:08 -0700

    Final comments

--- tree.cpp
+++ tree.cpp
@@ -37,6 +37,7 @@ In particular, this taught me about deletion in a Binary Tree and how the three 
 
 using namespace std;
 
+//Function Prototypes: 
 void fileIn(node**, char file[10]);
 void manIn(node**);
 void add(node*, node*);
@@ -44,92 +45,118 @@ void printTree(node*, int);
 void searchTree(node*, int, bool &);
 void del(node*, int);
 
+
+/*This function is the main function where the user will input commands to change the Binary Tree
+ (such as addition, printing the tree, deletion, and searching for elements in the tree)*/
 int main() {
+  bool editing = true; //this will loop the editing process (until the user quits)
+  char choice[15]; //user input
 
-  bool editing = true;
-  char choice[15];
+  //The initial head of the Binary Tree:
   node* head = new node(-1);
-  node** toHead = new node*;
+  node** toHead = new node*; //pointer to head
   (*toHead) = head;
-  cout << (*toHead) -> getData() << endl;
 
+  //In a loop, the user will be able to input commands:
   while (editing) {
-    cout << "Hi, there. Would you like to add to (add), print (print), delete from (del), or search (search) a Binary Tree? Or quit the program? " << endl;
+    cout << "Hi, there. Would you like to add to (add), print (print), delete from (del), or search (search) a Binary Tree? Or quit (quit) the program? " << endl;
     cin >> choice;
-
     cout << "OK" << endl;
-    
+
+    //Quit the program:
     if (!strcmp(choice, "quit")) {
-      editing = false;
+      editing = false; //end loop
     }
 
-    else if (!strcmp(choice, "create")) {
+    //Add to tree:
+    else if (!strcmp(choice, "add")) {
       char choice2[15];
       
       cout << "Would you like to read in numbers from a file (file) or input manually (man)? " << endl;
       cin >> choice2;
 
+      //Adding from a file:
       if (!strcmp(choice2, "file")) {
 	char file[10];
 	cout << "Please input the name of the file you want to read in: " << endl;
 	cin >> file;
-	fileIn(toHead, file);
+	fileIn(toHead, file); //read in from the file specified
       }
 
+      //Adding in manually:
       else if (!strcmp(choice2, "man")) {
 	cout << "Please input your numbers manually, seperated by spaces: " << endl;
-	manIn(toHead);
+	manIn(toHead); //begin manual addition process
       }
       cout << "Created Binary Search Tree!" << endl;
     }
 
+    //Printing the tree:
     else if (!strcmp(choice, "print")) {
       cout << "Here is the tree: " << endl;
-      //cout << (*toHead) -> getData() << endl;
-      //cout << (*toHead) -> getR() -> getData() << endl;
 
-      //cout << "here's the actual tree plz work" << endl;
+      //Caution: The tree could be empty (null head)
       if ((*toHead) == NULL) {
 	cout << "Tree is empty" << endl;
       }
+
+      //Caution: The tree could be empty (head value of -1 at beginning of code)
       else if ((*toHead)->getData() == -1) {
 	cout << "Tree is empty" << endl;
       }
+
+      //Otherwise (avoiding a seg fault), print the tree: 
       else {
 	printTree(*toHead, 0);
       }
     }
 
-    else if (!strcmp(choice, "delete_from")) {
-      int hated;
-
+    //Deleting from tree:
+    else if (!strcmp(choice, "del")) {
+      int hated; //value to delete
       cout << "What number do you want to delete? " << endl;
       cin >> hated;
 
-      cout << "check first" << endl;
-      if ((*toHead) -> getL() == NULL && (*toHead) -> getR() == NULL && (*toHead) -> getData() == hated) {
+      //Caution: The tree could be empty (null head)
+      if ((*toHead) == NULL) {
+	cout << "Tree is empty" << endl;
+      }
+
+      //Caution: The tree could be empty (head value of -1 at beginning of code)
+      else if ((*toHead) -> getData() == -1) {
+	cout << "Tree is empty" << endl;
+      }
+      
+      //Caution: If the head is the only value in the tree and needs to be deleted, just set it to null
+      else if ((*toHead) -> getL() == NULL && (*toHead) -> getR() == NULL && (*toHead) -> getData() == hated) {
       (*toHead) = NULL;
       }
-      else {
-	cout << "going in " << endl; 
+
+      //Otherwise, call the deletion function:
+      else { 
 	del((*toHead), hated);
       }
       cout << "Deleted number from Binary Search Tree!" << endl; 
     }
 
+    //Searching in tree:
     else if (!strcmp(choice, "search")) {
-      int wanted;
-      bool there = false;
-
+      int wanted; //value to find
+      bool there = false; //whether the value is there
       cout << "What number are you looking for? " << endl;
       cin >> wanted;
 
+      //Caution: The tree could be empty (null head)
       if ((*toHead) == NULL) {
-	cout << "Tree is empty" << endl;
+	cout << "Tree is empty, so" << endl;
       }
+
+      //Caution: The tree could be empty (head value of -1 at the beginning of code - state of head at start of program)
       else if ((*toHead) -> getData() == -1) {
-	cout << "Tree is empty" << endl; //state in beginning
+	cout << "Tree is empty, so" << endl;
       }
+
+      //Otherwise, check if value is in the tree:
       else {
 	searchTree(*toHead, wanted, there);
       }
@@ -146,52 +173,67 @@ int main() {
   return 0;
 }
 
+/*This fileIn function reads integers in a file whose name is specified by the user.
+  It takes in the file's name and a double pointer to a node in the Binary Tree
+  (this node will be the head, as done in the main function)
+ The file is assumed to have positive integers (1 - 999) seperated by spaces.*/
 void fileIn(node** head, char file[10]) {
 
+  //Referred to source from Udacity team for syntax on how to read a file at
   //https://www.udacity.com/blog/2021/05/how-to-read-from-a-file-in-cpp.html
-
-  cout << "getting file" << endl;
-  ifstream theFile(file);
-  int toAdd;
+  /*The source noted how one can create an ifstream class object using a constructor that takes in the file name.
+    Then, the source noted how using >> after the ifstream object will read in values until a space.
+    The source also mentioned how this action can be placed in a while loop (as the condition, even).
+   */
   
-  while (theFile >> toAdd) {
-    node* n = new node(toAdd);
+  ifstream theFile(file); //the file
+  int toAdd; //element to add to tree
 
+  //Loop through every value in the file and add it to tree:
+  while (theFile >> toAdd) {  
+    node* n = new node(toAdd); //create a node from the value gotten from the file
+
+    //Caution: The tree could be empty (null head)
     if ((*head) == NULL) {
-      (*head) = n;
+      (*head) = n; //in this case, set the head to the created node
     }
-    else if ((*head) -> getData() == -1) {
-      (*head) = n;
-      cout << "changed" << endl;
-    }
-    else {
-      cout << "adding to head" << endl;
-      add(*head, n);
-      cout << (*head) -> getR() -> getData() << endl;
 
-      if ((*head) -> getR() -> getR() != NULL) {
-	cout << (*head) -> getR() -> getR() -> getData() << endl;
-      }
+    //Caution: The tree could be empty (head value of -1 at the beginning of code)
+    else if ((*head) -> getData() == -1) {
+      (*head) = n; //set head to the created node
     }
-  }
-  
-  cout << "exited loop" << endl;
+
+    //Otherwise, call the add function:
+    else {
+      add(*head, n); //add node
+    }
+  }  
+  cout << "Read in from file" << endl;
 
 }
 
+
+/*This manIn function allows the user to type in a list of integers (assumed to be between 1 - 999) separated spaces.
+  The end of the list should be -1 for the list to go through. The function loops through the input, adding each integer to the Binary Tree. It takes in a double pointer to a node in the tree (which should be the head) to find this Binary Tree and add to it.
+ */
 void manIn(node** head) {
+  int toAdd; //value to add
 
-  int toAdd;
-  
-  while (cin >> toAdd && toAdd != -1) { //idea to use -1 to end input suggested by Kevin!
-    node* n = new node(toAdd);
+  //Continously allow the user to input numbers seperated by spaces until reaching -1 (>> reads until a space and this is looped):
+  while (cin >> toAdd && toAdd != -1) { //Idea to use -1 to end input, since I'm reading in positive integers, suggested by Kevin!
+    node* n = new node(toAdd); //create a node from the value gotten
 
+    //Caution: The tree could be empty (null head)
     if ((*head) == NULL) {
-      (*head) = n;
+      (*head) = n; //set the head to the input
     }
+
+    //Caution: The tree could be empty (head value of -1 at the beginning of code)
     else if ((*head) -> getData() == -1) {
-      (*head) = n;
+      (*head) = n; //set the head to the input
     }
+
+    //Otherwise, add the input:
     else {
       add(*head, n);
     }
@@ -199,158 +241,163 @@ void manIn(node** head) {
   
 }
 
+/*This add function takes in a pointer to a node in the tree and a node to add to the tree.
+It recursively moves down the tree until reaching the end of the tree, adding the specified node.
+It moves down right if the specified node is greater than or equal to the current node and moves down left if not. 
+*/
 void add(node* head, node* toAdd) {
 
+  //If cannot move down left in the tree anymore and value of the node-to-add is smaller than the current node:
   if (head -> getL() == NULL && toAdd -> getData() < head -> getData()) {
-    cout << "reached end, going to add" << endl;
-    (*head).setL(toAdd);
+    (*head).setL(toAdd); //add the node as the left child
     return;
   }
 
+  //If cannot move down right in the tree anymore and value of the node-to-add is >= than the current node:
   else if (head -> getR() == NULL && toAdd -> getData() >= head -> getData()) {
-    cout << "reached end, going to add R" << endl;
-    (*head).setR(toAdd);
+    (*head).setR(toAdd); //add the node as the right child
     return;
   }
 
+  //If node-to-add is smaller:
   if (toAdd -> getData() < head -> getData()) {
-    cout << "recursing left" << endl;
-    add(head -> getL(), toAdd);
+    add(head -> getL(), toAdd); //go down the tree left
   }
 
+  //If the node-to-add is >=:
   else if (toAdd -> getData() >= head -> getData()) {
-    cout << "recursing right" << endl;
-    add(head -> getR(), toAdd);
+    add(head -> getR(), toAdd); //go down the tree right
   }
-
-  //cout << head -> getData();
   return;
 }
 
+/*This printTree function recursively goes down the tree and prints it sideways (as such, it takes in a node as a point of reference in the tree). It also takes in an integer for depth down the tree to help indent elements when printing.*/
 void printTree(node* head, int depth) {
 
+  //When reaching an end (leaf) in the tree, print it out:
   if (head -> getL() == NULL && head -> getR() == NULL) {
+
+    //Print its depth: 
     for (int i = 0; i < depth; i++) {
-    cout << "\t";
+      cout << "\t";
     }
+
+    //Print the value:
     cout << "[";
     cout << head -> getData() << "]" << endl;
     return;
   }
 
-  //print right, parent, then left
+  //The idea for this recursive print is to print the right child, the parent, and then the left child:
+
+  //Go recursively down right: 
   if (head -> getR() != NULL) {
     printTree(head -> getR(), depth + 1);
   }
-  
+
+  //Print the current node:
   for (int i = 0; i < depth; i++) {
-    cout << "\t";
+    cout << "\t"; //print its depth
   }
-
   cout << "[";
-  cout << head -> getData() << "]" << endl;
+  cout << head -> getData() << "]" << endl; //print its value
 
+  //Go recursively down left:
  if (head -> getL() != NULL) {
    printTree(head -> getL(), depth + 1);
   }
 }
 
+/*This searchTree function takes in a wanted integer, the head of the tree, and a boolean.
+  It recursively goes down the tree (using the node in the argument) and checks to see if the current node has a value equal to the wanted integer. If so, it sets the boolean to true and otherwise, it sets it to false */
 void searchTree(node* head, int wanted, bool &there) {
 
-  cout << "looking at " << head -> getData() << endl;
+  //If the current node has the wanted value:
   if (head -> getData() == wanted) {
-    there = true;
+    there = true; //it is found!
     return;
   }
 
+  //Reaching the end of the tree, stop going down recursively:
   if (head -> getL() == NULL && head -> getR() == NULL) {
     return;
   }
 
+  //Go down right if wanted integer is bigger than current node:
   if (wanted > head -> getData() && head -> getR() != NULL) {
-    searchTree(head -> getR(), wanted, there); //recurse right
+    searchTree(head -> getR(), wanted, there);
   }
 
+  //Go down left if wanted integer is less than current node:
   if (wanted < head -> getData() && head -> getL() != NULL) {
     searchTree(head -> getL(), wanted, there); //recurse left
   }
-  
   return;
 }
 
+
+/* This del function deletes a specified number from the tree (it thus takes in a number to delete and a node in the tree to access the tree). First, it searches for a number that equals the number desired to be deleted. Then, there are three cases of deletion. For this function, I referred to Mr. Galbraith's summary of the workings of a Binary Tree (https://www.youtube.com/watch?v=h5zwiQjl69g).
+
+   This taught me about deletion in a Binary Tree and how the three cases of deletion are a) deleting a node with no children, b) deleting a node with one child, and c) deleting a node with two children. Additionally, when deleting the head of the tree (or nodes with two children, in general), it can be replaced with a "successor," the smallest node bigger than it. To access this successor, one can travel to the right of the head/current node and recursively go down left, while possible. When this is no longer possible, one has arrived at the successor.
+ */
 void del(node* head, int hated) {
 
-  //referred to https://www.youtube.com/watch?v=h5zwiQjl69g&t=112s (Mr. Galbraith's summary of a binary tree and deletion scenarios)
+  //Case 1: There is no children of the node-to-delete
 
+  //When the node to delete (equal to the desired number), with no children, is to the left of the current node:
   if (head -> getL() != NULL && head -> getL() -> getData() == hated && head -> getL() -> getL() == NULL && head -> getL() -> getR() == NULL) {
-    cout << "at left" << endl;
     node* prev = head -> getL();
-    cout << "set prev" << endl;
-    head -> setL(NULL);
-    cout << "Set" << endl;
-    delete prev; //case 1
-    cout << "delete" << endl;
+    head -> setL(NULL); //disconnect this node (a leaf) from the tree
+    delete prev; //delete the leaf
     return;
   }
 
+  //When the node to delete (equal to the desired number), with no children, is to the right of the current node:
   else if (head -> getR() != NULL && head -> getR() -> getData() == hated && head -> getR() -> getL() == NULL && head -> getR() -> getR() == NULL) {
-    cout << "at right" << endl;
     node* prev = head -> getR();
-    cout << "Set prev" << endl;
-    head -> setR(NULL);
-    cout << "set" << endl;
-    delete prev;
-    cout << "delete" << endl;
+    head -> setR(NULL); //disconnect node (as a leaf) from the tree by setting pointer to it, from parent, to NULL
+    delete prev; //delete leaf
     return;
   }
-  
+
   else if (head -> getData() == hated) {
-    cout << "found it" << endl;
+
+    //Case 2: Deleting a node with two children
     
-    /*if (head -> getL() == NULL && head -> getR() == NULL) {
-      cout << "case 1" << endl;
-      head = head -> getL();
-      
-      cout << "make null" << endl;
-      //delete (*head); //case one: deleting node with no children
-      cout << "deleted content" << endl;
-      return;
-      }*/
+    if (head -> getL() != NULL && head -> getR() != NULL) { //node to delete has two children
 
-    if (head -> getL() != NULL && head -> getR() != NULL) {
-      //case two: deleting node with two children
+      //Get the smallest node larger than the deleted node to replace it with:
+      node* replaceWith = head;
 
-      cout << "case 2" << endl;
-      
-      node* replaceWith = head; //get the smallest # larger than the deleted node to replace it with
+      //If there are nodes smaller than the node directly to the right of the node-to-be-deleted:
+      if (replaceWith -> getR() -> getL() != NULL) {
+	
+	replaceWith = head -> getR(); //go to node directly to the right
 
-      if (replaceWith -> getR() -> getL() != NULL) { //can go lower
-	replaceWith = head -> getR();
-
+	//While possible, go down to lower-value nodes (down left):
 	while (replaceWith -> getL() != NULL && replaceWith -> getL() -> getL() != NULL) {
 	  replaceWith = replaceWith -> getL();
 	}
 
-	cout << "reached the end at " << replaceWith -> getData() << endl;
-      
+	//Replace node-to-be-deleted with this node (its successor):
 	node* actual = new node;
 	*actual = *(replaceWith -> getL());
 	actual -> setL(head -> getL());
-	actual -> setR(head -> getR());
-	cout << "get actual" << endl;
-	
-	*head = *actual; //copy value (but don't change left or right)
-	cout << "copy" << endl;
-	
-	if (replaceWith -> getL() -> getR() == NULL) {
-	  replaceWith -> setL(NULL);
+	actual -> setR(head -> getR()); //has same children as node-to-be-deleted but with data of found node
+	*head = *actual; //copy
+
+	if (replaceWith -> getL() -> getR() == NULL) { //If the node moved to the deleted nodes position has no right child:
+	  replaceWith -> setL(NULL); //set it to null
 	}
-	else {
-	  *(replaceWith -> getL()) = *(replaceWith -> getL() -> getR());
+	
+	else {//Otherwise:
+	  *(replaceWith -> getL()) = *(replaceWith -> getL() -> getR()); //set it to its right child
 	}
       }
 
-      else { //cannot go lower
+      //If there are not nodes smaller than the node directly to the right of the node-to-be-deleted, use that node:
+      else {
+	//Get node-to-be-deleted's successor:
 	node* actual = new node;
 	*actual = *(replaceWith -> getR());
 	actual -> setL(head -> getL());
@@ -364,62 +411,40 @@ void del(node* head, int hated) {
 	  *(replaceWith -> getR()) = *(replaceWith -> getR() -> getR());
 	}
       }
-
-      //going down left to get successor
-	
-
-      cout << "replacements made" << endl;
       return;
     }
 
-    else { //case three: deleting node with one child
+    //Case Three: Deleting a node with one child
+    else {
 
-      cout << "case three" << endl;
-      
+      //If the node-to-be-deleted has a left child:
       if (head -> getL() != NULL) {
-	cout << "has l child" << endl;
-	//node* prevHead = head;
-	//prevHead -> setL(NULL);
-	//prevHead -> setR(NULL);
-	//cout << "created temp " << endl;
-	*(head) = *(head -> getL());
-	cout << "move on" << endl;
-	//delete prevHead;
-	cout << "deleted" << endl;
+	*(head) = *(head -> getL()); //replace it with its left child
       }
 
+      //If the node-to-be-deleted has a right child:
       else if (head -> getR() != NULL) {
-	cout << "right c" << endl;
-	//node* prevHead = head;
-	//node* instead = NULL;
-	//prevHead -> setL(instead);
-	//prevHead -> setR(instead);
-	//cout << "created temp " << endl;
-	*(head) = *(head -> getR());
-	cout << "move on" << endl;
-	//delete prevHead;
-	cout << "deleted" << endl;
+	*(head) = *(head -> getR()); //replace it with its right child
       }
       return;
     }
   }
 
+  //Searching the tree recursively:
+  
+  //If value desired is more than the current node, go down right:
   if (hated > head -> getData() && head -> getR() != NULL) {
-    //node** next = new node*;
-    //(*next) = (*head) -> getR();
     node* next = new node;
     next = head -> getR();
     del(next, hated);
   }
 
+  //If value desired is less than the current node, go down left:
   if (hated < head -> getData() && head -> getL() != NULL) {
-    //node** next = new node*;
-    //(*next) = (*head) -> getL();
     node* next = new node;
     next = head -> getL();
     del(next, hated);
   }
-
   return;
 }
 
